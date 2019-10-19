998f6efb4b895d47b88829ca256b0f683c7bd891
Lsze5821 <46796206+Lsze5821@users.noreply.github.com>
2019-10-19 18:13:38 -0400

    Linked Adopt Button

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 public class Controller {
+
+  @FXML private Button adopt_button;
+
+  public void initialize(){
+  adopt_button.setOnAction(this::handleButtonAction);
+  }
+
+  @FXML private void handleButtonAction(ActionEvent event){
+    System.out.println("Pet Adopted.");
+  }
 }
+
+
