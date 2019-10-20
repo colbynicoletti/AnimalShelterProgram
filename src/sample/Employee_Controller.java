50757238b5b5b25b11824091ccbf334c8d97febc
Lsze5821 <46796206+Lsze5821@users.noreply.github.com>
2019-10-20 08:05:29 -0400

    Added Species to Customers Search Box

--- src/sample/Employee_Controller.java
+++ src/sample/Employee_Controller.java
@@ -6,19 +6,36 @@ import javafx.scene.control.ComboBox;
 
 public class Employee_Controller {
 
-    @FXML
-    private ComboBox<?> cb_species;
+  @FXML
+  private ComboBox<Species> cb_species;
 
-    @FXML
-    private ComboBox<?> cb_breed;
+  @FXML
+  private ComboBox<?> cb_breed;
 
-    @FXML
-    private Button btn_checkIn;
+  @FXML
+  private Button btn_checkIn;
 
-    @FXML
-    private ComboBox<?> cb_event;
+  @FXML
+  private ComboBox<?> cb_event;
 
-    @FXML
-    private ComboBox<?> cb_date;
+  @FXML
+  private ComboBox<?> cb_date;
+
+  public void initialize() {
+    btn_checkIn.setOnAction(this::handleButtonAction);
+    cb_species();
+  }
+
+  private void handleButtonAction(javafx.event.ActionEvent actionEvent) {
+    System.out.println("Checked in");
+  }
+  private void cb_species(){
+    cb_species.getItems().addAll(Species.Dogs);
+    cb_species.getItems().addAll(Species.Cats);
+    cb_species.getItems().addAll(Species.Rabbits);
+    cb_species.getItems().addAll(Species.Monkeys);
+    cb_species.getItems().addAll(Species.Ferrets);
+
+  }
 
 }
