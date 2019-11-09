9df41014e3ad15af361a76104bd156022ec20022
Fernando Ramirez <feramirez5725@eagle.fgcu.edu>
2019-11-09 15:05:44 -0500

    Added aesthetic css file.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,10 +2,15 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -18,13 +23,56 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         primaryStage.setTitle("Animal Program");
-        primaryStage.setScene(new Scene(root, 600, 400));
-        primaryStage.show();
 
+        Scene scene = new Scene(root, 600, 400);
+        primaryStage.setScene(scene);
+        //Calling CSS file
+        scene.getStylesheets().add(getClass().getResource("Aesthetics .css").toExternalForm());
+        primaryStage.show();
+        //
 //        GridPane gridPane = new GridPane();
 //        gridPane.setPadding(new Insets(60));
     }
 
+    public static class databaseConnection {
+
+        private static final String jdbc_driver = "org.h2.Driver";
+        private static final String DB_url = "jdbc:h2:./res/AnimalShelter";
+
+        private static final String user = "";
+        private static final String pass = "";
+        private Connection conn = null;
+        private Statement stmt = null;
+
+        /**
+         * Method that starts the connection between the controller and the database.
+         */
+        public void connDatabase() {
+            try {
+                Class.forName(jdbc_driver);
+                conn = DriverManager.getConnection(DB_url, user, pass);
+                stmt = conn.createStatement();
+                System.out.println("Database Connection Established.");
+
+      /*
+      String sql = "SELECT * FROM JOBS";
+
+      ResultSet rs = stmt.executeQuery(sql);
+      while (rs.next()) {
+        System.out.println(rs.getString(1));
+      }
+
+       */
+                stmt.close();
+                conn.close();
+
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
 
 
         public static void main(String[] args) {
@@ -32,3 +80,4 @@ public class Main extends Application {
 
         }
     }
+}
