7bbd8373a2d39657392ca3a3e417acdcfe6b952d
Nickolas Gadomski <spudnick79@gmail.com>
2019-11-09 20:34:34 -0500

    New Commit and push

--- src/sample/Widget.java
+++ src/sample/Widget.java
@@ -1,7 +1,7 @@
 package sample;
 
 public class Widget extends AnimalType {
-    Widget(Species species, Breeds breeds, String petName, int animalID) {
+    Widget(Species species, Breeds breeds, String petName, String animalID) {
         super(species, breeds, petName, animalID);
     }
 
@@ -11,7 +11,7 @@ public class Widget extends AnimalType {
     }
 
     @Override
-    public String getBreed() {
+    public Breeds getBreed() {
         return null;
     }
 
