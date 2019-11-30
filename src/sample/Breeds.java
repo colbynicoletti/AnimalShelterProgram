28d98eec2d36aaf34871d5d6b5b4215f2345e377
dmauretour <dcmauretour6992@eagle.fgcu.edu>
2019-11-30 14:10:24 -0500

    Updated Version

---
+++ src/sample/Breeds.java
@@ -0,0 +1,21 @@
+package sample;
+
+public enum Breeds {
+    CAT,
+    Persion_Cat,
+    Russian_Blue,
+    Bengal_Cat,
+    Bristish_Shorthair,
+    Munchkin,
+    DOG,
+    Husky,
+    Pug,
+    Beagle,
+    Bulldog,
+    Black_Sable,
+    RABBITT,
+    French_Lop,
+    MONKEY,
+    Capuchin;
+
+}
