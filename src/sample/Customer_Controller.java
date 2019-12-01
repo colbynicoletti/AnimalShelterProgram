dd1c562fa1b94dac6bede57229afb05954243595
Lsze5821 <46796206+Lsze5821@users.noreply.github.com>
2019-12-01 13:58:23 -0500

    Manipulated Customer combobox

--- src/sample/Customer_Controller.java
+++ src/sample/Customer_Controller.java
@@ -1,30 +1,29 @@
 package sample;
 
-        import javafx.collections.FXCollections;
-        import javafx.collections.ObservableList;
-        import javafx.event.ActionEvent;
-        import javafx.fxml.FXML;
-        import javafx.fxml.FXMLLoader;
-        import javafx.scene.Node;
-        import javafx.scene.Parent;
-        import javafx.scene.Scene;
-        import javafx.scene.control.*;
-        import javafx.scene.input.MouseEvent;
-        import javafx.stage.Stage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
-        import java.io.IOException;
-        import java.util.ArrayList;
+import java.io.IOException;
 
 public class Customer_Controller {
 
     @FXML
-    private ChoiceBox<Species> speciesChoice;
+    private ComboBox<String> speciesCombo;
 
     @FXML
-    private ChoiceBox<Breeds> breedChoice;
+    private ComboBox<String> breedCombo;
 
     @FXML
-    private ChoiceBox<String> idChoice;
+    private ComboBox<String> idCombo;
     @FXML
     private ListView<AnimalType> displayAnimal;
     @FXML
@@ -49,29 +48,60 @@ public class Customer_Controller {
     @FXML
     private DatePicker dateAndTime;
 
+    private ObservableList<String> animalSpecies = FXCollections.observableArrayList("Dogs", "Cats", "Monkey", "Rabbit");
+    private ObservableList<String> dogBreeds = FXCollections.observableArrayList("Husky", "Chihuahua", "Beagle", "Pug", "Boston Terrier", "GreyHound", "Pomeranian", "Maltese", "Poodle", "Other");
+    private ObservableList<String> catBreeds = FXCollections.observableArrayList("Persion Cat", "Russian Blue", "Bengal Cat", "British Shorthair", "Munchkin", "Siamese Cat", "Ragdoll", "Other");
+    private ObservableList<String> monkeyBreeds = FXCollections.observableArrayList("Capuchin", "Guenon", "Macaque", "Tamarin", "Marmosets", "Other");
+    private ObservableList<String> rabbitBreeds = FXCollections.observableArrayList("Holland Lop", "Netherland Dwarf", "Flemish Giant", "Lionhead", "Rex", "Angora", "Other");
 
 
     public void initialize() {
-        initializeChoiceBox();
-        initializeChoiceBox1();
+        //initializeChoiceBox();
+        //initializeChoiceBox1();
         initializeChoiceBox2();
         //initializeComboBox3();
+        //speciesChoice();
+        speciesCombo.setValue("Species");
+        speciesCombo.setItems(animalSpecies);
+        breedCombo.setValue("Select Species");
+
+
     }
 
-    private void initializeChoiceBox() {
-
-        speciesChoice.getItems().addAll(Species.values());
-        speciesChoice.getSelectionModel().selectFirst();
-
+    @FXML
+    void breedChoice(ActionEvent event) {
+        breedCombo.setValue("Breeds");
+        if (speciesCombo.getValue().equals("Dogs")) {
+            breedCombo.setItems(dogBreeds);
+            System.out.println("print dog");
+        } else if (speciesCombo.getValue().equals("Cats")) {
+            breedCombo.setItems(catBreeds);
+            System.out.println("Print cat");
+        } else if (speciesCombo.getValue().equals("Monkey")) {
+            breedCombo.setItems(monkeyBreeds);
+            System.out.println("Print monkey");
+        } else if (speciesCombo.getValue().equals("Rabbit")) {
+            breedCombo.setItems(rabbitBreeds);
+            System.out.println("print rabbit");
+        } else {
+            System.out.println("Error");
+        }
     }
-    private void initializeChoiceBox1() {
-        breedChoice.getItems().addAll(Breeds.values());
-        breedChoice.getSelectionModel().selectFirst();
-    }
+//    private void initializeChoiceBox() {
+//
+//        speciesCombo.getItems().addAll(Species.values());
+//        speciesCombo.getSelectionModel().selectFirst();
+//
+//    }
+//
+//    private void initializeChoiceBox1() {
+//        breedCombo.getItems().addAll(Breeds.values());
+//        breedCombo.getSelectionModel().selectFirst();
+//    }
 
     private void initializeChoiceBox2() {
 
-        ObservableList<String> data = idChoice.getItems(); // Get item value input
+        ObservableList<String> data = idCombo.getItems(); // Get item value input
 
         //data.clear();
         data.add("DO12");
@@ -80,13 +110,13 @@ public class Customer_Controller {
         data.add("CA34");
         data.add("A12");
 //
-        idChoice.getSelectionModel().selectFirst();
+        idCombo.getSelectionModel().selectFirst();
     }
 
 
     @FXML
     void adoptButton(ActionEvent event) {
-
+        System.out.println("Adopted");
         // selectedAnimal.appendText(String.valueOf(displayAnimal.getSelectionModel().getSelectedItem()));
 
     }
